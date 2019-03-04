f2ab9ba1f0257bcbd4171fe1fa1cdf89daa2a9d3
Colin Reisterer <colin.reisterer@gmail.com>
2019-03-03 19:59:24 -0500

    Initial Xcode project

---
+++ MultiProjectStrings/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MultiProjectStrings
+//
+//  Created by Colin Reisterer on 3/3/19.
+//  Copyright © 2019 FullyTyped. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+}
